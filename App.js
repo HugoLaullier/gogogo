40a80c3ca9ce11e58fad261e71a1f2fc1b19567e
HugoLaullier <hugo.laullier@gmail.com>
2021-11-06 17:06:49 +0100

    start

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+import Home from './components/Home';
+import AddPlayers from './components/AddPlayers';
+
+const Stack = createStackNavigator();
+const navigatorOptions = {
+  headerShown: false
+};
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Stack.Navigator screenOptions={navigatorOptions} initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="AddPlayers" component={AddPlayers} />
+        </Stack.Navigator>
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingTop: StatusBar.currentHeight,
+    backgroundColor:"#f3b060"
+  }
 });
+
+
---
+++ constants/Color..js
@@ -0,0 +1,5 @@
+export default 
+{
+    orange:"#f3b060",
+    red:"#c70039"
+}
